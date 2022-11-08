065babd4b2dd2c7cec0463e4d8d7296d2a9a64bb
“mookbin” <beeen1@naver.com>
2022-11-08 20:21:15 +0900

    crud

--- Node_FoodV2/public/js/click.js
+++ Node_FoodV2/public/js/click.js
@@ -4,3 +4,29 @@ const tdClickHandler = (tag) => {
   const t_date = parentTR.dataset.t_date;
   document.location.href = `/`;
 };
+
+const tdClickHandlerV1 = (tag) => {
+  const target = tag.target;
+  if (target.tagName === "TD") {
+
+    const t_date = target.dataset.t_date;
+
+
+    document.location.href = `/food/detail/${t_date}`;
+  }
+};
+const tdClickHandlerV2 = (tag) => {
+  const target = tag.target;
+
+  const parentTR = target.closest("TR");
+
+  const t_date = parentTR.dataset.t_date;
+
+  document.location.href = `/food/${t_date}/detail`;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fdTable = document.querySelector("table.food.list");
+
+  fdTable?.addEventListener("click", tdClickHandlerV2);
+});
