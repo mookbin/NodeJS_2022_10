007ff31c7c341cb3f4f3c5a483bcc59634cf30df
mookbin <beeen1@naver.com>
2022-11-14 16:44:36 +0900

    erp

---
+++ Node_ERP/README.md
@@ -0,0 +1,29 @@
+# 나라상사 ERP 프로젝트
+
+- since 2022-11-11
+
+## 거래처 관리 프로젝트
+
+- ERP 프로젝트를 수행하기 위한 거래처정보를 등록, 수정, 삭제하는 기능 구현
+
+* Table 명세 설계, ERD(Entity Relationship Diagram) 작성
+* erpDBV2 database 생성, tbl_buyer table 생성
+* 프로젝트에서 사용할 model 파일을 생성하기
+
+```
+관리자 권한으로 cmd 창 열고
+npm install -g sequelize-auto
+npm install -g mysql2
+```
+
+```
+npm install
+sequelize-auto -d erpDBV2 -h localhost -u root -x !Biz8080
+```
+
+# gradient
+
+```
+http://cssgradient.io/
+
+```
---
+++ Node_ERP/routes/buyer.js
@@ -0,0 +1,34 @@
+import express from "express";
+import DB from "../models/index.js";
+
+//init-models 리턴 값에서 뽑아내기
+const Buyer = DB.models.tbl_buyer;
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const buyers = await Buyer.findAll();
+  res.render("buyer/list", { buyers });
+});
+
+router.get("/insert", (req, res) => {
+  res.render("buyer/write");
+});
+
+router.post("/insert", async (req, res) => {
+  // body 데이터 모두 뽑아내기
+  const data = req.body;
+  console.log(data);
+  try {
+    await Buyer.create(data);
+    res.redirect("/buyer");
+  } catch (err) {
+    console.error(err);
+    res.send("SQL 오류");
+  }
+});
+
+router.get("/detail", (req, res) => {
+  res.render("buyer/detail");
+});
+export default router;
