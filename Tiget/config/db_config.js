3b6c83b3962a8ff60532d8db5b5883c7f6884deb
mookbin <beeen1@naver.com>
2022-11-22 09:49:16 +0900

    MongoDB_Project

--- Tiget/config/db_config.js
+++ Tiget/config/db_config.js
@@ -1,7 +1,7 @@
 export default {
   development: {
     username: "root",
-    password: "Kang1414!!",
+    password: "!Biz8080",
     database: "erpdbv2",
     host: "127.0.0.1",
     dialect: "mysql",
