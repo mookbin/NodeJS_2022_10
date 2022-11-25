b04de772bac23c4eee04d6fc1a77d6b4fdf02f3b
mookbin <beeen1@naver.com>
2022-11-25 16:02:08 +0900

    22

---
+++ Node_BBS_V3/public/js/write.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnInput = document.querySelector("button.bbs.input");
+  const btnList = document.querySelector("button.bbs.list");
+  const formInput = document.querySelector("form.bbs.write");
+
+  btnInput?.addEventListener("click", () => {
+    // 유효성 검사 후
+    formInput?.submit();
+  });
+
+  btnList?.addEventListener("click", () => {});
+});
